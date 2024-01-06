2fd2cbb99af5c6bdcf2ac6f237446ae7a64af997
Per Stark <perstark.se@gmail.com>
2024-01-06 20:37:12 +0100

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <Providers>{children}</Providers>
       </body>
